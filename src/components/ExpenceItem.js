a0f6d6584f736d0e27a3a08533e64951cb88f3c5
Harshit Satya <harshitsatya5@gmail.com>
2022-01-13 12:26:52 +0530

    added props to first component

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,5 +1,19 @@
-function ExpenceItem() {
-  return <h2>Expence Item</h2>;
+import "./ExpenceItem.css";
+
+function ExpenceItem(props) {
+  const expenceDate = new Date(2022, 0, 1);
+  const expenceTitle = "Car Insurance";
+  const expeneceAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenceItem;
